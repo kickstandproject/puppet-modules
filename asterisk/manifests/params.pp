a4fa5c263af471e3aeefbeb7c1580fe1d96b515c
Paul Belanger <paul.belanger@polybeacon.com>
2011-11-21 19:25:05 -0500

    Remove unsed variables
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- asterisk/manifests/params.pp
+++ asterisk/manifests/params.pp
@@ -14,24 +14,4 @@ class asterisk::params {
 	$hasstatus = $operatingsystem ? {
 		default => true,
 	}
-
-	$configfile = $operatingsystem ? {
-		default => '/etc/asterisk/asterisk.conf',
-	}
-
-	$configfile_mode = $operatingsystem ? {
-		default => '640',
-	}
-
-	$configfile_owner = $operatingsystem ? {
-		default => 'asterisk',
-	}
-
-	$configfile_group = $operatingsystem ? {
-		default => 'asterisk',
-	}
-
-	$configdir = $operatingsystem ? {
-		default => '/etc/asterisk',
-	}
 }
