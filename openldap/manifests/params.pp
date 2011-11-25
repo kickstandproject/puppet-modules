4a92dda1819d290785920e5715feb443879ce56b
Leif Madsen <leif@leifmadsen.com>
2011-11-25 09:05:36 -0500

    Add the openldap base module.
    Signed-off-by: Leif Madsen <leif@leifmadsen.com>

---
+++ openldap/manifests/params.pp
@@ -0,0 +1,21 @@
+class openldap::params {
+	$packagename = $operatingsystem ? {
+		default => 'slapd',
+	}
+
+	$packagename_client = $operatingsystem ? {
+		default => 'ldap-utils',
+	}
+
+	$servicename = $operatingsystem ? {
+		default => 'slapd',
+	}
+
+	$processname = $operatingsystem ? {
+		default => 'slapd',
+	}
+
+	$hasstatus = $operatingsystem ? {
+		default => true,
+	}
+}
