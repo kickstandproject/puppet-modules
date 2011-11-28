6b14b157c25b995bffbbbbb5dc6d490110be0d06
Leif Madsen <leif@leifmadsen.com>
2011-11-28 09:38:23 -0500

    Resolve conflict.

--- openldap/manifests/classes/common.pp
+++ openldap/manifests/classes/common.pp
@@ -1,9 +1,6 @@
 class openldap::classes::common {
 	include openldap::params
 	include openldap::classes::install
-<<<<<<< HEAD
 	include openldap::classes::config
-=======
->>>>>>> a293784aee16cf955f80d761c4e4528d97cbef7b
 	include openldap::classes::service
 }
