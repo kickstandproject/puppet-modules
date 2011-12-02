c2e12d5545bba235d4eae9711615fd86e5feba52
Leif Madsen <leif@leifmadsen.com>
2011-12-02 09:24:15 -0500

    Update configuration to use the params.pp file per pabelanger.
    Signed-off-by: Leif Madsen <leif@leifmadsen.com>

--- openldap/manifests/classes/config.pp
+++ openldap/manifests/classes/config.pp
@@ -1,36 +1,36 @@
 class openldap::classes::config {
 
 	File {
-		owner	=> "root",
-		group	=> "root",
-		mode	=> 0644,
+		owner	=> "$dfl_owner",
+		group	=> "$dfl_group",
+		mode	=> "$dfl_mode",
 	}
 
-	file { "/etc/puppet/modules/openldap":
+	file { "$openldap_staging_path":
 		ensure	=> "directory",
 		mode	=> 0755,
 	}
 
-	file { "/etc/default/slapd":
+	file { "$openldap_operatingsystem_slapd_config":
 		ensure	=> present,
 		source	=> "puppet:///modules/openldap/etc/default/slapd",
 		require	=> Class["openldap::classes::install"],
 		notify	=> Class["openldap::classes::service"],
 	}
 
-	file { "/etc/ldap/slapd.conf":
+	file { "$openldap_slapd_conf":
 		ensure	=> present,
 		content	=> template('openldap/slapd.erb'),
 		require	=> Class["openldap::classes::install"],
 		notify	=> Class["openldap::classes::service"],
 	}
 
-	file { "/etc/puppet/modules/openldap/frontend.ldif":
+	file { "$openldap_frontend_ldif":
 		ensure	=> present,
 		content	=> template('openldap/frontend.erb'),
 	}
 
-	file { "/etc/puppet/modules/openldap/initial_config.sh":
+	file { "$openldap_initial_config_script":
 		ensure	=> present,
 		source	=> "puppet:///modules/openldap/initial_config.sh",
 		mode	=> 0500
@@ -38,7 +38,7 @@ class openldap::classes::config {
 
 	# This is really the better way to handle passing the passwd to ldapadd, but I can't make it work right now
 	#
-	file { "/etc/puppet/modules/openldap/.ldap_rootpw":
+	file { "$openldap_rootpw_file":
 		ensure	=> present,
 		content	=> template('openldap/rootpw.erb'),
 		require	=> Class["openldap::classes::install"],
@@ -46,11 +46,11 @@ class openldap::classes::config {
 	}
 
 	exec { "Initial LDAP Configuration":
-		subscribe	=> [ Package["slapd"], Package["ldap-utils"] ],
+		subscribe	=> [ Package["$packagename"], Package["$packagename_client"] ],
 		refreshonly	=> true,
-		cwd		=> "/etc/puppet/modules/openldap",
-		creates		=> "/etc/puppet/modules/openldap/.ldap_initial_configuration",
+		cwd		=> "$openldap_staging_path",
+		creates		=> "$openldap_staging_path/.ldap_initial_configuration",
 		path		=> "/bin:/usr/bin",
-		command		=> "/etc/puppet/modules/openldap/initial_config.sh $ldap_rootdn $ldap_domain $ldap_plain_passwd"
+		command		=> "$openldap_initial_config_script $ldap_rootdn $ldap_domain $ldap_plain_passwd"
 	}
 }
--- openldap/manifests/params.pp
+++ openldap/manifests/params.pp
@@ -18,4 +18,41 @@ class openldap::params {
 	$hasstatus = $operatingsystem ? {
 		default => true,
 	}
+
+	$dfl_owner = $operatingsystem ? {
+		default => 'root',
+	}
+
+	$dfl_group = $operatingsystem ? {
+		default =>  'root',
+	}
+
+	$dfl_mode = $operatingsystem ? {
+		default => 0644,
+	}
+
+	$openldap_staging_path = $operatingsystem ? {
+		default => '/etc/puppet/modules/openldap/',
+	}
+
+	$openldap_operatingsystem_slapd_config = $operatingsystem ? {
+		# TODO: this needs to be abstracted out for Ubuntu/Debian specifically
+		default => '/etc/default/slapd',
+	}
+
+	$openldap_slapd_conf = $operatingsystem ? {
+		default => '/etc/ldap/slapd.conf',
+	}
+
+	$openldap_frontend_ldif = $operatingsystem ? {
+		default => '/etc/puppet/modules/openldap/frontend.ldif',
+	}
+
+	$openldap_initial_config_script = $operatingsystem ? {
+		default => '/etc/puppet/modules/openldap/initial_config.sh',
+	}
+
+	$openldap_rootpw_file = $operatingsystem ? {
+		default => '/etc/puppet/modules/openldap/.ldap_rootpw',
+	}
 }
