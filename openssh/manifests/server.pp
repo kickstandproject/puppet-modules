b53e2346b71e78d181dc7a0d68de09a32fe1a8a9
Paul Belanger <paul.belanger@polybeacon.com>
2011-12-03 16:02:49 -0500

    Reformat openssh module
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- openssh/manifests/server.pp
+++ openssh/manifests/server.pp
@@ -1,10 +1,5 @@
 class openssh::server {
-	include openssh::params
-	include openssh::classes::install
-	include openssh::classes::config
-	include openssh::classes::service
-
-	if $firewall == 'yes' {
-		include openssh::classes::firewall
+	if ($firewall == 'yes') {
+		include openssh::common::firewall
 	}
 }
