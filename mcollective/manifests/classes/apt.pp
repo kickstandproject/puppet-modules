6182fb7bb63c55bb5e7c3fadadba7382ddfd5374
Leif Madsen <leif@leifmadsen.com>
2011-12-13 09:00:28 -0500

    Just clean up the apt.pp file to use tabs, not spaces.
    Signed-off-by: Leif Madsen <leif@leifmadsen.com>

--- mcollective/manifests/classes/apt.pp
+++ mcollective/manifests/classes/apt.pp
@@ -1,16 +1,16 @@
 class mcollective::classes::apt {
 
-        apt::source { "lucid_universe":
-                location	=> "http://ca.archive.ubuntu.com/ubuntu/",
-                release		=> "lucid",
-                repos		=> "universe",
-        }
+	apt::source { "lucid_universe":
+		location	=> "http://ca.archive.ubuntu.com/ubuntu/",
+		release		=> "lucid",
+		repos		=> "universe",
+	}
 
-        apt::source { "lucid_updates_universe":
-                location	=> "http://ca.archive.ubuntu.com/ubuntu/",
-                release		=> "lucid-updates",
-                repos		=> "universe",
-        }
+	apt::source { "lucid_updates_universe":
+		location	=> "http://ca.archive.ubuntu.com/ubuntu/",
+		release		=> "lucid-updates",
+		repos		=> "universe",
+	}
 
 	apt::source { "puppetlabs":
 		location	=> "http://apt.puppetlabs.com/",
