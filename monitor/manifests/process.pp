b877c58932b20d27053f216b87c30dfde639e530
Paul Belanger <paul.belanger@polybeacon.com>
2012-01-18 10:55:12 -0500

    Coding Guidelines
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- monitor/manifests/process.pp
+++ monitor/manifests/process.pp
@@ -1,17 +1,17 @@
 define monitor::process(
-	$ensure	= present,
-	$process
-	) {
+    $ensure = present,
+    $process
+    ) {
 
-	if ($fqdn != $nagios_nsca_server) {
-		nagios::service::nsca { "check_procs_$process!1!1":
-			description	=> "Check Process $process",
-			ensure		=> $ensure,
-			server		=> "$nagios_nsca_server",
-		}
-	}
+    if (${fqdn} != ${nagios_nsca_server}) {
+        nagios::service::nsca { "check_procs_${process}!1!1":
+            description => "Check Process ${process}",
+            ensure      => $ensure,
+            server      => $nagios_nsca_server,
+        }
+    }
 
-	nagios::command { "check_procs_$process":
-		command_line	=> "/usr/lib/nagios/plugins/check_procs -C $process -w '\$ARG1$:' -c '\$ARG2$:'"
-	}
+    nagios::command { "check_procs_${process}":
+        command_line    => "/usr/lib/nagios/plugins/check_procs -C ${process} -w '\$ARG1$:' -c '\$ARG2$:'"
+    }
 }
