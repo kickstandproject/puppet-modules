34d433f228db0f4bf4398a7c7574a7d51f1d511a
Paul Belanger <paul.belanger@polybeacon.com>
2012-01-25 14:16:10 -0500

    Disable 'default' site by default
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- apache/manifests/common/config.pp
+++ apache/manifests/common/config.pp
@@ -14,4 +14,16 @@ class apache::common::config {
     file { $apache::params::rootdir:
         ensure  => directory,
     }
+
+    file { "${apache::params::rootdir}/index.html":
+        ensure  => absent,
+        require => File[$apache::params::rootdir],
+    }
+
+    exec { "apache_a2dissite_default":
+        command => "/usr/sbin/a2dissite default",
+        onlyif => "/usr/bin/test -f ${apache::params::configdir}/sites-enabled/000-default",
+        notify  => Class['apache::common::service'],
+        require => Class['apache::common::install'],
+    }
 }
