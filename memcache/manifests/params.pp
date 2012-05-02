f46173bb030370df670a12b14d8738ae5c8fbe40
Paul Belanger <paul.belanger@polybeacon.com>
2012-05-02 15:20:18 -0400

    Create memcache module Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ memcache/manifests/params.pp
@@ -0,0 +1,52 @@
+#
+# puppet-modules: The Kickstand Project
+#
+# Copyright (C) 2011, Polybeacon, Inc.
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+# See http://kickstand-project.org for more information about
+# the Kickstand project. Please do not directly contact any
+# of the maintainers of this project for assistance; the
+# project provides a web site, forums and IRC channels for
+# your use.
+#
+# This program is free software, distributed under the terms
+# of the GNU General Public License Version 2. See the LICENSE
+# file at the top of the source tree.
+#
+class memcache::params {
+    $configfile = $::operatingsystem ? {
+        default => '/etc/memcached.conf',
+    }
+
+    $group = $::operatingsystem ? {
+        default => 'memcache',
+    }
+
+    $hasstatus = $::operatingsystem ? {
+        default => true,
+    }
+
+    $mode = $::operatingsystem ? {
+        default => '0644',
+    }
+
+    $owner = $::operatingsystem ? {
+        default => 'memcache',
+    }
+
+    $packagename = $::operatingsystem ? {
+        default => 'memcached',
+    }
+
+    $processname = $::operatingsystem ? {
+        default => 'memcached',
+    }
+
+    $servicename = $::operatingsystem ? {
+        default => 'memcached',
+    }
+}
+
+# vim:sw=4:ts=4:expandtab:textwidth=79
