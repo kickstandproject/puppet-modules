529e37621788594d39f1c5355f641387bee6ec76
Paul Belanger <paul.belanger@polybeacon.com>
2012-05-02 16:24:32 -0400

    Add missing header and footer Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- rabbitmq/manifests/common/install.pp
+++ rabbitmq/manifests/common/install.pp
@@ -1,7 +1,25 @@
-class rabbitmq::common::install {
-    require rabbitmq::params
+#
+# puppet-modules: The Kickstand Project
+#
+# Copyright (C) 2011, Polybeacon, Inc.
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+# See http://kickstand-project.org for more information about
+# the Kickstand project. Please do not directly contact any
+# of the maintainers of this project for assistance; the
+# project provides a web site, forums and IRC channels for
+# your use.
+#
+# This program is free software, distributed under the terms
+# of the GNU General Public License Version 2. See the LICENSE
+# file at the top of the source tree.
+#
 
-	package { $rabbitmq::params::packagename:
-		ensure	=> present,
-	}
+class rabbitmq::common::install {
+    package { $rabbitmq::params::packagename:
+        ensure  => present,
+    }
 }
+
+# vim:sw=4:ts=4:expandtab:textwidth=79
