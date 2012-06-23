348bc3dddbc24b5720deaebf4ea9412647803566
Paul Belanger <paul.belanger@polybeacon.com>
2012-06-23 14:22:34 -0400

    Use kickstandstand-common repo
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- common/manifests/concat/client.pp
+++
@@ -1,6 +0,0 @@
-class common::concat::client {
-    include common::concat::params
-    include common::concat::client::init
-}
-
-# vim:sw=4:ts=4:expandtab:textwidth=79
--- common/manifests/concat/client/init.pp
+++
@@ -1,5 +0,0 @@
-class common::concat::client::init {
-    include common::concat::client::config
-}
-
-# vim:sw=4:ts=4:expandtab:textwidth=79
