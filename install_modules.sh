925edaf9676bf186c96d285a3f7cc9c6e33756b1
Paul Belanger <paul.belanger@polybeacon.com>
2012-10-07 18:43:43 -0400

    Bump kickstandproject-timezone to 0.0.3
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- install_modules.sh
+++ install_modules.sh
@@ -10,7 +10,7 @@ MODULES["kickstandproject-network"]="0.0.1"
 MODULES["kickstandproject-ntp"]="0.0.3"
 MODULES["kickstandproject-puppet"]="0.0.2"
 MODULES["kickstandproject-ssh"]="0.0.3"
-MODULES["kickstandproject-timezone"]="0.0.2"
+MODULES["kickstandproject-timezone"]="0.0.3"
 MODULES["puppetlabs-stdlib"]="2.2.0"
 
 MODULE_LIST=`puppet module list`
