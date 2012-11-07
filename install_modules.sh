9425da3e5deb792626592cdac60cf5e538e9aa97
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-07 17:07:38 -0500

    Use kickstandproject-dahdi 0.0.2 module
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- install_modules.sh
+++ install_modules.sh
@@ -6,6 +6,7 @@ MODULE_PATH=/etc/puppet/modules
 
 declare -A MODULES
 MODULES["kickstandproject-apt"]="0.0.1"
+MODULES["kickstandproject-dahdi"]="0.0.2"
 MODULES["kickstandproject-jenkins"]="0.0.1"
 MODULES["kickstandproject-jenkins_job_builder"]="0.0.2"
 MODULES["kickstandproject-network"]="0.0.1"
