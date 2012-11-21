5660adac94aaf48b60e0e201dc26c766fbd09334
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-21 14:52:23 -0500

    Rework our git module
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/git/manifests/client.pp
+++ modules/git/manifests/client.pp
@@ -1,23 +1,6 @@
-#
-# puppet-modules: The Kickstand Project
-#
-# Copyright (C) 2011, Polybeacon, Inc.
-#
-# Paul Belanger <paul.belanger@polybeacon.com>
-#
-# See http://kickstand-project.org for more information about
-# the Kickstand project. Please do not directly contact any
-# of the maintainers of this project for assistance; the
-# project provides a web site, forums and IRC channels for
-# your use.
-#
-# This program is free software, distributed under the terms
-# of the GNU General Public License Version 2. See the LICENSE
-# file at the top of the source tree.
-#
 class git::client {
-    include git::params::client
-    include git::client::init
+  include git::params
+  include git::client::init
 }
 
-# vim:sw=4:ts=4:expandtab:textwidth=79
+# vim:sw=2:ts=2:expandtab:textwidth=79
